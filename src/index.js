72d8d5d4291f32fbadcd5b789cd2a70b7f2aaa37
ibk2510 <ibrahimkalil.18cs@kcct.ac.in>
2021-05-19 13:38:15 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// env --->
+env.config();
+
+//router
+const authRoutes = require('./routes/auth');
+
+//mongodb connect
+mongoose.connect(
+  ` mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@cluster0.u4lph.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex : true }
+)
+.then(
+    ()=>{
+        console.log("db connected");
+    }
+);
+
+//middlewares to handle request bodies
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use('/api' , authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log("server is running ", process.env.PORT);
+});
---
+++ src/models/user.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 3,
+    max: 20,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 3,
+    max: 20,
+  },
+  username:{
+      required : true,
+      trim : true,
+      unique : true,
+      index : true,
+      lowercase : true ,
+      type : String
+  },
+  email :{
+      type : String,
+      required : true ,
+      trim : true ,
+      unique : true,
+      lowercase : true 
+  },
+  hashPassword :{
+      type: String,
+      required : true,
+  },
+  role : {
+      type : String ,
+      enum : ['user' , 'farmer' , 'admin'],
+      default : 'user'
+  },
+  contactNumber : {
+      type : String, 
+
+  },
+
+},
+{
+    timestamps : true
+});
+
+
+userSchema.virtual('password').set(function(password){
+    this.hashPassword = bcrypt.hashSync(password , 10);
+});
+
+userSchema.methods = {
+    authenticate : function(password){
+        return bcrypt.compareSync(password , this.hashPassword);
+    }
+}
+
+
+module.exports = mongoose.model("User" , userSchema);
